edd4d9d83c9f05e977e34f5587e2acbff390292e
sbas17 <115462642+sbas17@users.noreply.github.com>
2023-02-05 19:25:56 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,48 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
 
-    public static void main (String[] args) {
+    public static void main (String[] args) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+
 
         File src = new File("E://NWork/Netology/JavaCore/_3.1/Games/src");
         if (src.mkdir())
-            System.out.println("Каталог " + src.getName() + " создан");
+            stringBuilder.append("Каталог " + src.getName() + " создан");
 
         File res = new File("E://NWork/Netology/JavaCore/_3.1/Games/res");
         if (res.mkdir())
-            System.out.println("Каталог " + res.getName() + " создан");
+            stringBuilder.append(" Каталог " + res.getName() + " создан");
+
+
 
         File saveGames = new File("E://NWork/Netology/JavaCore/_3.1/Games/savegames");
         if (saveGames.mkdir())
-            System.out.println("Каталог " + saveGames.getName() + " создан");
+            stringBuilder.append(" Каталог " + saveGames.getName() + " создан");
+
 
         File temp = new File("E://NWork/Netology/JavaCore/_3.1/Games/temp");
         if (temp.mkdir())
-            System.out.println("Каталог " + temp.getName() + " создан");
+            stringBuilder.append(" Каталог " + temp.getName() + " создан");
+
 
         File main = new File(src, "main");
         if (main.mkdir())
-            System.out.println("Каталог " + main.getName() + " создан");
+            stringBuilder.append(" Каталог " + main.getName() + " создан");
+
 
         File test = new File(src, "test");
         if (test.mkdir())
-            System.out.println("Каталог " + test.getName() + " создан");
+            stringBuilder.append(" Каталог " + res.getName() + " создан");
+
 
         File fileMain = new File(main, "Main.java");
         try {
             if (fileMain.createNewFile())
-                System.out.println("Файл " + fileMain.getName() + " был создан");
+                stringBuilder.append(" Файл " + fileMain.getName() + " был создан");
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
@@ -40,29 +50,35 @@ public class Main {
         File utils = new File(main, "Utils.java");
         try {
             if (utils.createNewFile())
-                System.out.println("Файл " + utils.getName() + " был создан");
+                stringBuilder.append(" Файл " + utils.getName() + " был создан");
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
 
         File drawables = new File(res, "drawables");
         if (drawables.mkdir())
-            System.out.println("Каталог " + drawables.getName() + " создан");
+            stringBuilder.append(" Каталог " + drawables.getName() + " создан");
 
         File vectors = new File(res, "vectors");
         if (vectors.mkdir())
-            System.out.println("Каталог " + vectors.getName() + " создан");
+            stringBuilder.append(" Каталог " + vectors.getName() + " создан");
 
         File icons = new File(res, "icons");
         if (icons.mkdir())
-            System.out.println("Каталог " + icons.getName() + " создан");
+            stringBuilder.append(" Каталог " + icons.getName() + " создан");
 
         File fileTemp = new File(temp, "temp.txt");
         try {
             if (fileTemp.createNewFile())
-                System.out.println("Файл " + fileTemp.getName() + " был создан");
+                stringBuilder.append(" Файл " + fileTemp.getName() + " был создан");
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
+
+        String data = stringBuilder.toString();
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("E://NWork/Netology/JavaCore/_3.1/Games/temp/temp.txt"));
+        bufferedWriter.write(data);
+        bufferedWriter.flush();
+        bufferedWriter.close();
     }
 }
